8a279ad788decc0561ada72fbc359a7a81819224
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2023-05-10 11:13:49 +0200

    Tabs

--- angular-starter-master/src/app/app.module.ts
+++ angular-starter-master/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,6 +10,8 @@ import { TranslocoRootModule } from './transloco-root.module';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatInputModule } from '@angular/material/input';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @NgModule({
   declarations: [
@@ -22,6 +25,7 @@ import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/mater
     MatSlideToggleModule,
     MatFormFieldModule,
     MatInputModule,
+    MatTabsModule
   ],
   providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}],
 
