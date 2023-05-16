b0951aa0cf6c428b0aaa2f25b9e29a646a9748c3
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2023-05-16 11:36:51 +0200

    Grid View T2P

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -38,7 +39,8 @@ const routes: Routes = [
     MatTabsModule,
     MatStepperModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}],
   bootstrap: [AppComponent]
