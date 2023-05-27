e0313cbfbfc7cd676d04a855b6eca8256a2ecda8
Manuel <81485512+ManuelsKopf@users.noreply.github.com>
2023-05-28 01:54:25 +0200

    inserted new iFrame for BPMN.io

--- src/app/t2p/t2p.component.ts
+++ src/app/t2p/t2p.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-t2p',
   templateUrl: './t2p.component.html',
   styleUrls: ['./t2p.component.css'],
-
 })
-
 export class T2PComponent {
   protected warningText: string = '';
+  iFrameURL: SafeResourceUrl;
+  displayIframe = false;
+  constructor(private sanitizer: DomSanitizer) {}
 
-
-
-
+  onOpenIFrame(): void {
+    this.iFrameURL =
+      this.sanitizer.bypassSecurityTrustResourceUrl('https://bpmn.io/');
+    this.displayIframe = true;
+  }
 }
-
