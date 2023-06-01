8b752b80b8d74763b58996c7b69aa454b595ef7c
Manuel <81485512+ManuelsKopf@users.noreply.github.com>
2023-06-01 11:42:14 +0200

    took it local

--- src/app/p2tHttpService.ts
+++ src/app/p2tHttpService.ts
@@ -6,7 +6,7 @@ import { throwError } from 'rxjs';
 
 const httpOptions= {headers: new HttpHeaders({
   'Content-Type':  'application/json',
-  Authorization: 'my-auth-token'
+  // Authorization: 'my-auth-token'
 })
 };
 
@@ -14,17 +14,24 @@ const httpOptions= {headers: new HttpHeaders({
   providedIn: 'root'
 })
 export class p2tHttpService {
+  // private url= 'localhost:8080/p2t/generateText'
   private url= 'https://woped.dhbw-karlsruhe.de/p2t/generateText'
 
   constructor(private p2thttp: HttpClient) {
 
    }
-   
+
    postP2T(text: string){
-      return this.p2thttp.post<string>(this.url, text, httpOptions)
-      //.pipe(catchError(this.handleError('0'))) 
-      //@felixschempfTODO 
-      
+      return this.p2thttp.post<string>(this.url, text, httpOptions).subscribe((response:any) =>{
+        const body = response.body;
+        console.log (body);
+      },
+      (error:any)=>{
+
+      });
+      //.pipe(catchError(this.handleError('0')))
+      //@felixschempfTODO
+
    }
    private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
@@ -34,7 +41,7 @@ export class p2tHttpService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error
-       
+
     }
     // Return an observable with a user-facing error message.
     return throwError(() => new Error('Something bad happened; please try again later.'));
@@ -43,4 +50,13 @@ export class p2tHttpService {
   // getP2t(){
   //   this.p2thttp.get(this.url).
   // }
+  // getConfig (resp => {
+  //   // display its headers
+  //   const keys = resp.headers.keys();
+  //   this.headers = keys.map(key =>
+  //     ${key}: ${resp.headers.get(key)});
+
+  //   // access the body directly, which is typed as Config.
+  //   this.config = { ...resp.body! };
+  // })
 }
--- src/app/t2p/t2p.component.ts
+++ src/app/t2p/t2p.component.ts
@@ -11,11 +11,33 @@ export class T2PComponent {
   protected warningText: string = '';
   iFrameURL: SafeResourceUrl;
   displayIframe = false;
+  protected text: string = '';
   constructor(private sanitizer: DomSanitizer) {}
 
   onOpenIFrame(): void {
     this.iFrameURL =
       this.sanitizer.bypassSecurityTrustResourceUrl('https://bpmn.io/');
     this.displayIframe = true;
+  }
+
+  public readUploadedFile(file: File): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        const fileContent = event.target.result;
+        resolve(fileContent);
+      };
+
+      reader.onerror = (event: any) => {
+        reject(event.target.error);
+      };
+
+      reader.readAsText(file);
+    });
+  }
+
+  onCLickButtonToFillOutName() {
+    console.log('Eingabe in Textfeld:' + this.text);
   }
 }
