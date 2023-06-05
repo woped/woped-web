199b9931bfcd75d2871191162aa7516b933298bc
Manuel <81485512+ManuelsKopf@users.noreply.github.com>
2023-06-05 12:27:58 +0200

    commit

--- src/app/t2p/t2p.component.ts
+++ src/app/t2p/t2p.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { t2pHttpService } from '../t2pHttpService';
 
 @Component({
   selector: 'app-t2p',
@@ -12,7 +14,8 @@ export class T2PComponent {
   iFrameURL: SafeResourceUrl;
   displayIframe = false;
   protected text: string = '';
-  constructor(private sanitizer: DomSanitizer) {}
+
+  constructor(private sanitizer: DomSanitizer, private http: t2pHttpService) {}
 
   onOpenIFrame(): void {
     this.iFrameURL =
@@ -20,24 +23,132 @@ export class T2PComponent {
     this.displayIframe = true;
   }
 
-  public readUploadedFile(file: File): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      const reader = new FileReader();
+  // public readUploadedFile(file: File): Promise<string> {
+  //   return new Promise<string>((resolve, reject) => {
+  //     const reader = new FileReader();
 
-      reader.onload = (event: any) => {
-        const fileContent = event.target.result;
-        resolve(fileContent);
-      };
+  //     reader.onload = (event: any) => {
+  //       const fileContent = event.target.result;
+  //       resolve(fileContent);
+  //     };
 
-      reader.onerror = (event: any) => {
-        reject(event.target.error);
-      };
+  //     reader.onerror = (event: any) => {
+  //       reject(event.target.error);
+  //     };
 
-      reader.readAsText(file);
-    });
-  }
+  //     reader.readAsText(file);
+  //   });
+  // }
 
   onCLickButtonToFillOutName() {
     console.log('Eingabe in Textfeld:' + this.text);
+  }
+  generateProcess(){
+    console.log("Generate Process")
+    const text= `<?xml version="1.0" encoding="UTF-8"?>
+    <pnml xmlns="pnml.woped.org">
+        <net type="http://www.informatik.hu-berlin.de/top/pntd/ptNetb" id="noID">
+            <place id="p2">
+                <name>
+                    <text>start</text>
+                    <graphics>
+                        <offset x="0" y="0"/>
+                    </graphics>
+                </name>
+                <graphics>
+                    <position x="0" y="0"/>
+                    <dimension x="40" y="40"/>
+                </graphics>
+                <initialMarking>
+                    <text>1</text>
+                </initialMarking>
+            </place>
+            <place id="p3">
+                <name>
+                    <text>end</text>
+                    <graphics>
+                        <offset x="0" y="0"/>
+                    </graphics>
+                </name>
+                <graphics>
+                    <position x="0" y="0"/>
+                    <dimension x="40" y="40"/>
+                </graphics>
+            </place>
+            <transition id="t2">
+                <name>
+                    <text>buy auto</text>
+                    <graphics>
+                        <offset x="0" y="0"/>
+                    </graphics>
+                </name>
+                <graphics>
+                    <position x="0" y="0"/>
+                    <dimension x="40" y="40"/>
+                </graphics>
+                <toolspecific tool="WoPeD" version="1.0">
+                    <trigger id="" type="200">
+                        <graphics>
+                            <position x="0" y="0"/>
+                            <dimension x="24" y="22"/>
+                        </graphics>
+                    </trigger>
+                    <transitionResource organizationalUnitName="all" roleName="manager">
+                        <graphics>
+                            <position x="0" y="0"/>
+                            <dimension x="60" y="22"/>
+                        </graphics>
+                    </transitionResource>
+                    <time>0</time>
+                    <timeUnit>1</timeUnit>
+                    <orientation>1</orientation>
+                </toolspecific>
+            </transition>
+            <arc id="a4" source="t2" target="p3">
+                <inscription>
+                    <text>1</text>
+                    <graphics>
+                        <offset x="500.0" y="-12.0"/>
+                    </graphics>
+                </inscription>
+                <toolspecific tool="WoPeD" version="1.0">
+                    <probability>1.0</probability>
+                    <displayProbabilityOn>false</displayProbabilityOn>
+                    <displayProbabilityPosition x="500.0" y="12.0"/>
+                </toolspecific>
+            </arc>
+            <arc id="a5" source="p2" target="t2">
+                <inscription>
+                    <text>1</text>
+                    <graphics>
+                        <offset x="500.0" y="-12.0"/>
+                    </graphics>
+                </inscription>
+                <toolspecific tool="WoPeD" version="1.0">
+                    <probability>1.0</probability>
+                    <displayProbabilityOn>false</displayProbabilityOn>
+                    <displayProbabilityPosition x="500.0" y="12.0"/>
+                </toolspecific>
+            </arc>
+            <toolspecific tool="WoPeD" version="1.0">
+                <bounds>
+                    <position x="2" y="25"/>
+                    <dimension x="763" y="574"/>
+                </bounds>
+                <scale>100</scale>
+                <treeWidthRight>549</treeWidthRight>
+                <overviewPanelVisible>false</overviewPanelVisible>
+                <treeHeightOverview>100</treeHeightOverview>
+                <treePanelVisible>false</treePanelVisible>
+                <verticalLayout>false</verticalLayout>
+                <resources>
+                    <role Name="manager"/>
+                    <organizationUnit Name="all"/>
+                </resources>
+            </toolspecific>
+        </net>
+    </pnml>`
+
+    this.http.postt2p(text)
   }
 }
--- src/app/t2pHttpService.ts
+++ src/app/t2pHttpService.ts
@@ -7,28 +7,31 @@ import {
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    // Authorization: 'my-auth-token'
+    Accept: 'text/plain, */*',
+    'Content-Type': 'text/plain', // We send Text
   }),
+  responseType: 'text' as 'json', // We accept plain text as response.
 };
 
 @Injectable({
   providedIn: 'root',
 })
 export class t2pHttpService {
-  private url= 'http://localhost:8080/t2p/generateText'
+  private url = 'http://localhost:8080/p2t/generateText';
   // private url = 'https://woped.dhbw-karlsruhe.de/t2p/generateText';
-  private text: string;
+  //private text: string;
   constructor(private t2phttpClient: HttpClient) {}
-  postt2p() {
+  postt2p(text: string) {
     return this.t2phttpClient
-      .post<string>(this.url, this.text, httpOptions)
+      .post<string>(this.url, text, httpOptions)
       .subscribe(
         (response: any) => {
           const body = response.body;
           console.log(body);
         },
-        (error: any) => {}
+        (error: any) => {
+          console.log(error);
+        }
       );
   }
 }
