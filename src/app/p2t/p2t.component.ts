b0e467823289c6ab86dcba3e9ee8b5cf39f5216a
Max Finkenbeiner <48648552+Runlay@users.noreply.github.com>
2023-06-29 17:21:20 +0200

    Display Methoden ausgelagert

--- src/app/p2t/p2t.component.ts
+++ src/app/p2t/p2t.component.ts
@@ -3,10 +3,20 @@ import { p2tHttpService } from '../Services/p2tHttpService';
 import { MatStepper } from '@angular/material/stepper';
 import { HttpResponse } from '@angular/common/http';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { defer, first, fromEvent, merge, mergeMap, switchMap, takeUntil, tap, windowWhen } from 'rxjs';
-import { SpinnerService } from '../Services/SpinnerService'
+import {
+  defer,
+  first,
+  fromEvent,
+  merge,
+  mergeMap,
+  switchMap,
+  takeUntil,
+  tap,
+  windowWhen,
+} from 'rxjs';
+import { SpinnerService } from '../Services/SpinnerService';
 import { t2pHttpService } from '../Services/t2pHttpService';
-
+import { ModelDisplayer } from '../utilities/modelDisplayer';
 
 //global variable to store the file content
 declare global {
@@ -20,9 +30,10 @@ declare global {
   selector: 'app-p2t',
   templateUrl: './p2t.component.html',
   styleUrls: ['./p2t.component.css'],
-  template: `'<p>p2t works!</p>'`
+  template: `'
+    <p>p2t works!</p>
+    '`,
 })
-
 export class P2tComponent {
   response: any;
   test: String;
@@ -34,7 +45,7 @@ export class P2tComponent {
   // ViewChild decorator to get a reference to the drop zone element
   @ViewChild('dropZone', { static: true }) dropZone: ElementRef<HTMLDivElement>;
 
-  isFileDropped: boolean = false
+  isFileDropped: boolean = false;
   droppedFileName: string = '';
 
   // ViewChild decorator to get a reference to the HTML file input element
@@ -49,43 +60,44 @@ export class P2tComponent {
     private p2tHttpService: p2tHttpService,
     private t2phttpService: t2pHttpService,
     public spinnerService: SpinnerService
-    ) {}
+  ) {}
 
-
-// This method generates the text based on the selected file type and content.Allows only pnml and bpmn files
+  // This method generates the text based on the selected file type and content.Allows only pnml and bpmn files
   generateText() {
     const paragraph = document.createElement('p');
-  if (this.fileType == "bpmn")
-    this.t2phttpService.displayBPMNModel(window.dropfileContent)
-  else if (this.fileType == "pnml")
-    this.t2phttpService.generatePetriNet(window.dropfileContent)
-    if (window.fileContent !== undefined || window.dropfileContent !== undefined) {
+    if (this.fileType == 'bpmn')
+      ModelDisplayer.displayBPMNModel(window.dropfileContent);
+    else if (this.fileType == 'pnml')
+      ModelDisplayer.displayPNMLModel(window.dropfileContent);
+    if (
+      window.fileContent !== undefined ||
+      window.dropfileContent !== undefined
+    ) {
       this.spinnerService.show();
-    //  this.p2tHttpService.postP2T(window.fileContent);
-      this.p2tHttpService.postP2T(window.dropfileContent)
-    }
-
-    else {
-      this.p2tHttpService.displayText("No files uploaded");
+      //  this.p2tHttpService.postP2T(window.fileContent);
+      this.p2tHttpService.postP2T(window.dropfileContent);
+    } else {
+      this.p2tHttpService.displayText('No files uploaded');
     }
     event.preventDefault();
 
     this.stepper.next();
   }
 
-
-   // This method is called when files are dropped on the drop zone
+  // This method is called when files are dropped on the drop zone
   onDrop(event: DragEvent) {
     event.preventDefault();
     const files = event.dataTransfer?.files;
     if (files && files.length > 0) {
       const allowedExtensions = ['pnml', 'bpmn'];
-      const hasAllowedFiles = Array.from(files).some(file => {
-        const fileExtension = file.name.substring(file.name.lastIndexOf('.') + 1).toLowerCase();
+      const hasAllowedFiles = Array.from(files).some((file) => {
+        const fileExtension = file.name
+          .substring(file.name.lastIndexOf('.') + 1)
+          .toLowerCase();
         return allowedExtensions.includes(fileExtension);
       });
       if (hasAllowedFiles) {
-        console.log("Hallo");
+        console.log('Hallo');
 
         this.processDroppedFiles(files);
         this.isFileDropped = true;
@@ -95,7 +107,7 @@ export class P2tComponent {
       }
     }
   }
-// Process the dropped files and read their contents
+  // Process the dropped files and read their contents
   processDroppedFiles(files: FileList) {
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
@@ -103,20 +115,21 @@ export class P2tComponent {
       const reader = new FileReader();
 
       reader.onload = (e) => {
-
         window.dropfileContent = reader.result as string;
       };
       reader.readAsText(file);
     }
   }
 
-
-// Download the generated text as a txt file
+  // Download the generated text as a txt file
   downloadText() {
     let text = this.p2tHttpService.getText();
-    let filename = "p2t";
+    let filename = 'p2t';
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute(
+      'href',
+      'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+    );
     element.setAttribute('download', filename);
 
     element.style.display = 'none';
@@ -126,12 +139,12 @@ export class P2tComponent {
     document.body.removeChild(element);
   }
 
-// Trigger the file input to select files
+  // Trigger the file input to select files
   selectFiles() {
     this.fileInputRef.nativeElement.click();
   }
 
- // Handle the file selection event
+  // Handle the file selection event
   onFileSelected(event: Event) {
     const fileInput = event.target as HTMLInputElement;
     const files = fileInput.files;
@@ -141,12 +154,12 @@ export class P2tComponent {
       this.droppedFileName = files[0].name;
 
       const allowedExtensions = ['pnml', 'bpmn'];
-      const hasAllowedFiles = Array.from(files).some(file => {
-        const fileExtension = file.name.substring(file.name.lastIndexOf('.') + 1).toLowerCase();
-        if (fileExtension == 'pnml')
-          this.fileType = "pnml";
-        else if(fileExtension == 'bpmn')
-          this.fileType = "bpmn";
+      const hasAllowedFiles = Array.from(files).some((file) => {
+        const fileExtension = file.name
+          .substring(file.name.lastIndexOf('.') + 1)
+          .toLowerCase();
+        if (fileExtension == 'pnml') this.fileType = 'pnml';
+        else if (fileExtension == 'bpmn') this.fileType = 'bpmn';
         return allowedExtensions.includes(fileExtension);
       });
       if (hasAllowedFiles) {
@@ -158,5 +171,4 @@ export class P2tComponent {
       }
     }
   }
-
 }
