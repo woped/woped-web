f5c496b0ea9b898d31e9c699c8741e63a014e66b
Christopher Caspari <caspari.christop.a21@student.dhbw-Karlsruhe.de>
2024-05-13 12:44:31 +0200

    Update app.module.ts
    Comment has been removed, component name "P2TComponent has been changed to P2tComponent to prevent misdefinition.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { p2tHttpService } from './Services/p2tHttpService';
 import { t2pHttpService } from './Services/t2pHttpService';
+import { AppComponent } from './app.component';
 
-import { RouterModule, Routes } from '@angular/router';
-import { T2PComponent } from './t2p/t2p.component';
-import { HomeComponent } from './home/home.component';
-import { P2tComponent } from './p2t/p2t.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatStepperModule } from '@angular/material/stepper';
 import {
   MAT_FORM_FIELD_DEFAULT_OPTIONS,
   MatFormFieldModule,
 } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { P2tComponent } from './p2t/p2t.component';
+import { T2PComponent } from './t2p/t2p.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 't2p', component: T2PComponent },
-  // { path: 'p2t', component: P2TComponent },
+  { path: 'p2t', component: P2tComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
