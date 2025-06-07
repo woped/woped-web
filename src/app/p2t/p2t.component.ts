82a7d87cb8991ecfddf8cba49afec995ea42f64d
DaveCodes26 <felix.david.chr@gmail.com>
2025-06-07 17:28:33 +0200

    added new needed variables to p2tComponent class

--- src/app/p2t/p2t.component.ts
+++ src/app/p2t/p2t.component.ts
@@ -33,6 +33,9 @@ export class P2tComponent implements OnInit {
   apiKeyExample = 'sk-proj-ABcdEFghIJklMNopQRstUVwxYZabCDefghIJklMNopQRstu';
   showPromptInput = false;
   useLLM = false;
+  useRag: boolean = false;
+  providers: string[] = ['openai', 'gemini', 'lmStudio']; 
+  selectedProvider: string = 'openai'; // Default provider
   prompt = `Create a clearly structured and comprehensible continuous text from the given BPMN that is understandable for an uninformed reader. The text should be easy to read in the summary and contain all important content; if there are subdivided points, these are integrated into the text with suitable sentence beginnings in order to obtain a well-structured and easy-to-read text. Under no circumstances should the output contain sub-items or paragraphs, but should cover all processes in one piece!`;
   isPromptReadonly = true;
   models: string[] = [];
